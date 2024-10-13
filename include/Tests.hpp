1854750b5584cba33c8c0fb307c82078e6d12fb6
Murat Toprak <murattoprak2004@gmail.com>
2024-10-13 15:07:18 +0300

    change test classes to be more robust - implement BasicArithmeticTest, TranscendentalTest, SpecialCasesTest   and PrecisionTest member functions to measure CPU's floating point arithmetic more   correctly - add more complexity to IntegerArithmeticTest class to get more   consistent results - define ITERATION_COUNT in IntegerArithmeticTest instead of using   BENCHMARK_ITERATION_COUNT, because it took too much time with the   increased complexity

--- include/Tests.hpp
+++ include/Tests.hpp
@@ -7,6 +7,15 @@ public:
 
 	void Run() override;
 	void RunSingleIteration() override;
+
+private:
+	constexpr static size_t ITERATION_COUNT = 100'000;
+	
+	constexpr static int64_t MULTIPLIER = 1'103'515'245;
+	constexpr static int64_t INCREMENT = 12345;
+	constexpr static int64_t MASK = 0x7fffffff;
+
+	int64_t PerformOperations(int64_t result);
 };
 
 class FloatingPointTest : public BenchmarkTest {
@@ -14,6 +23,12 @@ public:
 	FloatingPointTest();
 	void Run() override;
 	void RunSingleIteration() override;
+
+private:
+	benchmark_float_type BasicArithmeticTest();
+	benchmark_float_type TranscendentalTest();
+	benchmark_float_type SpecialCasesTest();
+	benchmark_float_type PrecisionTest();
 };
 
 class PrimeTest : public BenchmarkTest {
--- src/Tests.cpp
+++ src/Tests.cpp
@@ -1,45 +1,133 @@
 #include <cstdlib>
 #include <functional>
+#include <random>
+#include <array>
 
+#include "Logger.hpp"
 #include "Tests.hpp"
 
+/* TODO: Declare the variable based on compilation options(volatile or not) */
+
+/* Integer Arithmetic Test Class */
 IntegerArithmeticTest::IntegerArithmeticTest() 
 	: BenchmarkTest("Integer Arithmetic Test") {}
 
 void IntegerArithmeticTest::Run() {
-	volatile int result = 0;
-	for (int i = 0; i < BENCHMARK_ITERATION_COUNT; ++i) {
-		result += i * (i + 1) / 2;
+	int64_t result = 0;
+	for (size_t i = 0; i < ITERATION_COUNT; ++i) {
+		result += (static_cast<int64_t>(i) * (i + 1)) / 2;
+		result = PerformOperations(result);
 	}
+	
 	if (result == 0) {
+		g_logger->Error("Integer arithmetic test result is 0");
 		throw BenchmarkException("Unexpected result in Integer Arithmetic Test");
 	}
 }
 
 void IntegerArithmeticTest::RunSingleIteration() {
-	volatile int result = 0;
-	result += rand() * (rand() + 1) / 2;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int64_t> dist(0, RAND_MAX);
+
+	int64_t result = 0;
+	result += (dist(gen) * (dist(gen) + 1)) / 2;
+	result += PerformOperations(result);
+}
+
+int64_t IntegerArithmeticTest::PerformOperations(int64_t result) {
+	result *= MULTIPLIER;
+	result += INCREMENT;
+	result &= MASK;
+	return result;
 }
 
 
+/* Floating Point Test Class */
 FloatingPointTest::FloatingPointTest()
 	: BenchmarkTest("Floating Point Test") {}
 
 void FloatingPointTest::Run() {
-	volatile benchmark_float_type result = 0.0;
-	for (int i = 0; i < BENCHMARK_ITERATION_COUNT; ++i) {
-		result += std::sqrt(static_cast<benchmark_float_type>(i) * 3.14159);
-	}
-	if (std::isnan(result)) {
-		throw BenchmarkException("NaN result in Floating-Point Arithmetic Test");
+	benchmark_float_type result = 0.0;
+	result += BasicArithmeticTest();
+	result += TranscendentalTest();
+	result += SpecialCasesTest();
+	result += PrecisionTest();
+
+	if (std::isnan(result) || std::isinf(result)) {
+		throw BenchmarkException("Invalid result in Floating Point Test");
 	}
 }
 
 void FloatingPointTest::RunSingleIteration() {
+	static std::mt19937 gen(std::random_device{}());
+	static std::uniform_real_distribution<benchmark_float_type> dis(-1000.0, 1000.0);
+
+	benchmark_float_type x = dis(gen);
+	benchmark_float_type y = dis(gen);
+
 	volatile benchmark_float_type result = 0.0;
-	result += std::sqrt(static_cast<benchmark_float_type>(rand()) * 3.14159);
+	result += std::sin(x) * std::cos(y);
+	result += std::exp(std::fmod(x, 5.0));
+	result += std::log(std::abs(x) + 1.0);
+	result += std::sqrt(std::abs(x * y));
 }
 
+benchmark_float_type FloatingPointTest::BasicArithmeticTest() {
+	benchmark_float_type result = 0.0;
+	for (int i = 0; i < BENCHMARK_ITERATION_COUNT; ++i) {
+		benchmark_float_type x = static_cast<benchmark_float_type>(i) * 0.1;
+		result += x + x;
+		result -= x * x;
+		result *= 1.01;
+		result /= 1.01;
+	}
+	return result;
+}
+
+benchmark_float_type FloatingPointTest::TranscendentalTest() {
+	benchmark_float_type result = 0.0;
+	for (int i = 0; i < BENCHMARK_ITERATION_COUNT; ++i) {
+		benchmark_float_type x = static_cast<benchmark_float_type>(i) * 0.1;
+		result += std::sin(x) * std::cos(x);
+		result += std::exp(std::fmod(x, 5.0));
+		result += std::log(x + 1.0);
+		result += std::sqrt(std::abs(x));
+	}
+	return result;
+}
+
+benchmark_float_type FloatingPointTest::SpecialCasesTest() {
+	std::array<benchmark_float_type, 6> specialValues = {
+		0.0,
+		std::numeric_limits<benchmark_float_type>::epsilon(),
+		std::numeric_limits<benchmark_float_type>::min(),
+		std::numeric_limits<benchmark_float_type>::max(),
+		std::numeric_limits<benchmark_float_type>::infinity(),
+		std::numeric_limits<benchmark_float_type>::quiet_NaN()
+	};
+
+	benchmark_float_type result = 0.0;
+	for (auto val : specialValues) {
+		result += std::isfinite(val) ? val : 0.0;
+		result += std::isnormal(val) ? 1.0 : 0.0;
+		result += std::fpclassify(val);
+	}
+	return result;
+}
+
+benchmark_float_type FloatingPointTest::PrecisionTest() {
+	benchmark_float_type result = 0.0;
+	benchmark_float_type small = 1.0;
+	for (int i = 0; i < 100; ++i) {
+		small /= 10.0;
+		result += 1.0 + small;
+	}
+	return result - 100.0; // close to zero
+}
+
+
+/* Prime Test Class */
 PrimeTest::PrimeTest()
 	: BenchmarkTest("Prime Test") {}
 
